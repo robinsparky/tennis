42bb889ea25d6d8f823897a7b1ec34ed541afd56
robinsparky <robin.sparky@gmail.com>
2021-01-03 16:31:01 -0500

    Gave up trying to show debug backtrace

--- wp-plugins/tenniscommonlib/Baselogger.php
+++ wp-plugins/tenniscommonlib/Baselogger.php
@@ -15,12 +15,63 @@ class BaseLogger {
     public $recipient = "robin.sparky@gmail.com";
     
 	public function __construct( $log = true ) {
-        $loc = __CLASS__ . '::' . __FUNCTION__;
+        global $TennisEventNoLog;
+        if( isset( $TennisEventNoLog) ) $log = false;
         $this->writelog = $log;
     }
-    
-    public function error_log( $something, $label = '' ) {
+
+    public function error_log() {
+        $something = null;
+        $label = '';
+        $backtrace = false;
+        $numargs = func_num_args();
+        $arg_list = func_get_args();
+
         if( $this->writelog ) {
+            switch( $numargs ) {
+                case 0:
+                    return;
+                case 1:
+                    if( is_object( $arg_list[0]) || is_array($arg_list[0])) {
+                        $something = $arg_list[0];
+                        $label = '';
+                        $backtrace = false;
+                    }
+                    else{
+                        $something = null;
+                        $label = $arg_list[0];
+                        $backtrace = false;
+                    }
+                    break;
+                case 2:
+                    if( is_object( $arg_list[0] ) || is_array( $arg_list[0] ) ) {
+                        $something = $arg_list[0];
+                        if( is_string($arg_list[1]) ) {
+                            $label = $arg_list[1];
+                            $backtrace = false;
+                        }
+                        else {
+                            $label = '';
+                            $backtrace = $arg_list[1];
+                        }
+                    }
+                    else {
+                        $something = null;
+                        $label = $arg_list[0];
+                        $backtrace = $arg_list[1];
+                    }
+                    break;
+                case 3:
+                    $something = $arg_list[0];
+                    $label = $arg_list[1];
+                    $backtrace = $arg_list[2];
+                    break;
+                default:
+                $something = $arg_list[0];
+                $label = $arg_list[1];
+                $backtrace = $arg_list[2];
+            }
+
             if( is_object( $something ) || is_array( $something ) ) {
                 if( ! empty( $label ) ) error_log($label);
                 error_log( print_r( $something, true ) );
@@ -30,8 +81,14 @@ class BaseLogger {
                 error_log( "$label $something" );
             }
             else {
-                $label =  empty( $label ) ? "" : $label . ":";
-                error_log("$label <<<<<uknown>>>>");
+                //$label =  empty( $label ) ? "" : $label . ":";
+                error_log("$label");
+            }
+
+            $backtrace = false;
+            if( $backtrace ) {
+                error_log("****Backtrace:");
+                error_log( print_r($this->get_debug_trace(), true ) );
             }
         }
     }
@@ -49,6 +106,17 @@ class BaseLogger {
             error_log( $message, 1, $this->recipient 
                      , "Subject: $subject\nFrom: log@care4nurses.org\n");
         }
+    }
+    
+    private function get_debug_trace( $frames = 4) {
+ 
+        $frames += 2;
+        $trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, $frames );
+        array_shift($trace);
+        array_shift($trace);
+        array_shift($trace);
+
+        return $trace;
     }
 
 } //end of class
