7e9a7c932df1942e36fd1495d7fb0f7c7c48fbb0
robinsparky <robin.sparky@gmail.com>
2021-05-10 14:03:35 -0400

    Added more gender types

--- wp-plugins/tennisevents/includes/datalayer/class-gendertype.php
+++ wp-plugins/tennisevents/includes/datalayer/class-gendertype.php
@@ -11,11 +11,15 @@ class GenderType {
     public const MALES   = "males";
     public const FEMALES = "females";
     public const MIXED   = "mixed";
+    public const BOYS    = "boys";
+    public const GIRLS   = "girls";
 
     public static function AllTypes() {
-        return [  sprintf("%s", self::MALES)   => __('Males', TennisEvents::TEXT_DOMAIN )
-                , sprintf("%s", self::FEMALES) => __('Females', TennisEvents::TEXT_DOMAIN )
-                , sprintf("%s", self::MIXED)   => __('Mixed', TennisEvents::TEXT_DOMAIN ) ];
+        return [  sprintf("%s", self::MALES)   => __("Men", TennisEvents::TEXT_DOMAIN )
+                , sprintf("%s", self::FEMALES) => __("Women", TennisEvents::TEXT_DOMAIN )
+                , sprintf("%s", self::MIXED)   => __('Mixed', TennisEvents::TEXT_DOMAIN )
+                , sprintf("%s", self::BOYS)   => __('Boys', TennisEvents::TEXT_DOMAIN )
+                , sprintf("%s", self::GIRLS)   => __('Girls', TennisEvents::TEXT_DOMAIN ) ];
     }
 
     public static function isValid( $gt ) {
