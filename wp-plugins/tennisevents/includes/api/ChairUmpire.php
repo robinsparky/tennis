39a7252795b64e944a67d9aa5364941b684977ff
robinsparky <robin.sparky@gmail.com>
2021-07-28 18:41:54 -0400

    Fixed bug where no winner could no winner is found because of missing namespace on Entrant.

--- wp-plugins/tennisevents/includes/api/ChairUmpire.php
+++ wp-plugins/tennisevents/includes/api/ChairUmpire.php
@@ -465,7 +465,7 @@ abstract class ChairUmpire
         $winner = $this->matchWinner( $match );
 
         $wname = 'no winner yet';
-        if( is_a( $winner, 'Entrant' ) ) {
+        if( is_a( $winner, 'datalayer\Entrant' ) ) {
             $wname = $winner->getName();
         }
         elseif( is_string( $winner ) ) {
