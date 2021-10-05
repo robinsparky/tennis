83b754aed1634ffe0e925f51c5c05934d31a5ed3
robinsparky <robin.sparky@gmail.com>
2021-10-05 16:02:00 -0400

    Fixed formatting issue with digital clock

--- wp-plugins/tennisevents/js/digitalclock.js
+++ wp-plugins/tennisevents/js/digitalclock.js
@@ -14,23 +14,18 @@
       //res = d.toDateString();
       let ampm = d.getHours() >= 12 ? " pm" : " am";
       let hours = d.getHours() <= 12 ? d.getHours() : d.getHours() - 12;
-      res =
-        res +
-        " - " +
-        hours +
-        ":" +
-        d.getMinutes() +
-        ":" +
-        d.getSeconds() +
-        ":" +
-        ampm;
+      let minutes =
+        d.getMinutes() <= 10 ? "0" + d.getMinutes() : d.getMinutes();
+      let seconds =
+        d.getSeconds() <= 10 ? "0" + d.getSeconds() : d.getSeconds();
+      res = res + " " + hours + ":" + minutes + ":" + seconds + ampm;
 
       //return [year, month, day].join("-");
       return res;
     }
 
     function displayDigitalClock() {
-      let dtString = Date();
+      let dtString = Date().toString();
       document.getElementById("digiclock").innerHTML = formatDate(dtString);
     }
 
