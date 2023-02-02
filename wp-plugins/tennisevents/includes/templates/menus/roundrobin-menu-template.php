a889cd552c8fac69d51ccb996863fe79660da371
Robin Smith <robin.sparky@gmail.com>
2023-02-02 08:41:23 -0500

    Removed "switch players" menu item from round robin

--- wp-plugins/tennisevents/includes/templates/menus/roundrobin-menu-template.php
+++ wp-plugins/tennisevents/includes/templates/menus/roundrobin-menu-template.php
@@ -12,7 +12,6 @@
  <li><a class="undomatch">Undo Scores</a></li>
 </ul>
 <ul class="matchaction unapproved">
- <li class="switchplayers">Switch Players</li>
  <li><a class="setmatchstart">Start Date &amp; Time</a></li>
  <li><a class="setcomments">Comment Match</a></li>
 </ul>
